39060bb5df00203f20a7477c027bd15960e37726
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2016-09-21 17:07:10 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+    Application: require("./lib/Application.js"),
+    Module: require("./lib/Module.js"),
+    Script: require("./lib/Script.js"),
+    Tools: require("./lib/Tools.js")
+}
